a8ba43dce4a60ce1212d6db8a1fbe2ea1cbf0640
Penina Waghalter <waghalte@mail.yu.edu>
2022-12-29 15:14:05 -0500

    added cypher

--- lc3tools/lc3.f
+++ lc3tools/lc3.f
@@ -938,20 +938,21 @@ generate_instruction (operands_t operands, const char* opstr)
         write_value (inst.ccode | 0x001); // BRnzp three lines so the saved lines don't execute 
         write_value (0x0000); // basically a .blkw - save this spot so we can save temp_r1 here
 
-        int store_spot = 0x1E; // #30
+        int store_spot = 0x1B; // #30
         for (int i=0x00; i<0x0A; i++) {
             write_value (0xF023);
-            write_value (0x1020 | (0 << 9) | (0 << 6) | (0x04 & 0x1F)); // ADD shift to R0
-            write_value (0x3000 | (0 << 9) | (store_spot - 0x011 + i)); // ST R0 to just after code in mem
-            printf("%d\n", store_spot - 3 + i);
+            write_value (0x1020 | (0 << 9) | (0 << 6) | (0x04 & 0x1F)); // ADD 4 to R0
+            write_value (0x3000 | (0 << 9) | (store_spot - (3*i) + i)); // ST R0 to just after code in mem
+            printf("%x\n", store_spot - 0x011 + i);
+            printf("%x\n", i);
         }
 
-        for (int i=0; i<10; i++) {
+        for (int i=0; i<11; i++) {
             write_value(0x000);
         }
 
-        write_value (0xE000 | (0 << 9) | (0xFF5 & 0x1FF)); // LEA R0, #1
-        write_value (0xF024); // write the string to the console
+        write_value (0xE000 | (0 << 9) | (0xFF4 & 0x1FF)); // LEA R0, #1
+        write_value (0xF022); // write the string to the console
         
 
         break;
